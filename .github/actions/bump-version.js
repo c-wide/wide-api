acbee784552da106dc45106b058cb9cffde6d95b
c-wide <53365073+c-wide@users.noreply.github.com>
2022-12-08 11:15:24 -0600

    fix: no global or multiline for version action regex

--- .github/actions/bump-version.js
+++ .github/actions/bump-version.js
@@ -6,6 +6,6 @@ const fileNames = ['fxmanifest.lua', 'package.json'];
 
 fileNames.forEach((fileName) => {
   const file = fs.readFileSync(fileName, { encoding: 'utf8' });
-  const newFileContent = file.replace(/(?<=version.*)\d\.\d\.\d/gm, newVersion);
+  const newFileContent = file.replace(/(?<=version.*)\d\.\d\.\d/, newVersion);
   fs.writeFileSync(fileName, newFileContent);
 });
